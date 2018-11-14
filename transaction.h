ef3c7f2df27eb66f6f02ec993b20b171a137e56e
hillersc <42871127+hillersc@users.noreply.github.com>
2018-11-13 16:44:44 -0800

    Update transaction.h

--- transaction.h
+++ transaction.h
@@ -6,11 +6,16 @@
 class Transaction
 {
 public:
+    // Transaction constructors
     Transaction();
     Transaction(int day, double amount, std::string descrip);
+    // Reads in values for Transaction object
     void read();
+    // Return day for Transaction object
     int get_day();
+    // Return amount/balance for Transaction object
     double get_amount();
+    // Return description for Transaction object
     std::string get_descrip();
 private:
     int newDay;
