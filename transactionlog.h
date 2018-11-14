653fbb941ff0d2c3ad9e8d500ddc382643a4e27b
hillersc <42871127+hillersc@users.noreply.github.com>
2018-11-13 16:45:37 -0800

    Update transactionlog.h

--- transactionlog.h
+++ transactionlog.h
@@ -7,11 +7,17 @@
 class Transactionlog
 {
 public:
+    // Default constructor
     Transactionlog();
+    // Read in Transaction objects
     void read();
+    // Find amount in a vector of Transaction objects
     void amount(std::vector<Transaction> &t);
+    // Print vector that contains all Transactions
     void print_vector();
+    // Return the minimum daily balance of month
     double min_daily_bal(const std::vector<double> &t);
+    // Return the average daily balance of the month
     double avg_daily_bal(const std::vector<double> &t);
 
 private:
