a1bd3b7455f6abf8c4db92648148ab67443f7dda
hillersc <42871127+hillersc@users.noreply.github.com>
2018-11-13 16:44:17 -0800

    Update transaction.cpp

--- transaction.cpp
+++ transaction.cpp
@@ -1,12 +1,14 @@
 #include "transaction.h"
 #include <iostream>
 
+// Initialize default constructor values to zero
 Transaction::Transaction()
 {
     newDay = 0;
     newAmount = 0.0;
 }
 
+// Initialize object's values to user's parameters
 Transaction::Transaction(int day, double amount, std::string descrip)
 {
     newDay = day;
@@ -14,6 +16,7 @@ Transaction::Transaction(int day, double amount, std::string descrip)
     newDescrip = descrip;
 }
 
+// Read in user values to put in Transaction object
 void Transaction::read()
 {
     std::cout << "Day: ";
@@ -25,16 +28,19 @@ void Transaction::read()
     std::getline(std::cin, newDescrip);
 }
 
+// Return day as an int
 int Transaction::get_day()
 {
     return newDay;
 }
 
+// Return amount/balance as double
 double Transaction::get_amount()
 {
     return newAmount;
 }
 
+// Return description as a string
 std::string Transaction::get_descrip()
 {
     return newDescrip;
